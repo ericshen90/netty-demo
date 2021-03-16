f0329542fcc4bb9255123727e5216c79281f4260
EricShen <ahsbt@126.com>
2021-03-17 00:08:02 +0800

    first commit

---
+++ src/main/java/com/eric/netty/io/BIOEchoServer.java
@@ -0,0 +1,60 @@
+package com.eric.netty.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Java BIO
+ *
+ * @author EricShen
+ * @date 2021-03-16
+ */
+@Slf4j
+public class BIOEchoServer {
+
+
+    public static void main(String[] args) throws IOException {
+        // 启动服务端,绑定端口
+        ServerSocket serverSocket = new ServerSocket(8001);
+        log.info("start server");
+        while (true) {
+            // 接收服务端连接
+            Socket socket = serverSocket.accept();
+
+            log.info("on client conn: {}", socket);
+
+            // 新建线程处理连接
+            new Thread(() -> {
+                try {
+                    // 读取数据
+                    InputStream inputStream = socket.getInputStream();
+
+                    InputStreamReader bufferedInputStream = new InputStreamReader(inputStream);
+
+                    BufferedReader bufferedReader = new BufferedReader(bufferedInputStream);
+
+                    String msg;
+
+                    while (Objects.nonNull((msg = bufferedReader.readLine()))) {
+                        // 打印内容
+                        log.info("received msg: {}", msg);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }).start();
+
+
+        }
+
+    }
+
+}
---
+++ src/test/java/com/eric/netty/NettyApplicationTests.java
@@ -0,0 +1,66 @@
+package com.eric.netty;
+
+import io.netty.util.concurrent.DefaultEventExecutor;
+import io.netty.util.concurrent.DefaultPromise;
+import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+import io.netty.util.concurrent.Promise;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    public static void main(String[] args) {
+
+        // 构造线程池
+        EventExecutor executor = new DefaultEventExecutor();
+
+        // 创建 DefaultPromise 实例
+        Promise promise = new DefaultPromise(executor);
+
+        // 下面给这个 promise 添加两个 listener
+        promise.addListener(new GenericFutureListener<Future<Integer>>() {
+            @Override
+            public void operationComplete(Future future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("任务结束，结果：" + future.get());
+                } else {
+                    System.out.println("任务失败，异常：" + future.cause());
+                }
+            }
+        }).addListener(new GenericFutureListener<Future<Integer>>() {
+            @Override
+            public void operationComplete(Future future) throws Exception {
+                System.out.println("任务结束，balabala...");
+            }
+        });
+
+        // 提交任务到线程池，五秒后执行结束，设置执行 promise 的结果
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                    // promise.setFailure(new RuntimeException());
+                } catch (InterruptedException e) {
+                }
+                // 设置 promise 的结果
+                // promise.setFailure(new RuntimeException());
+                promise.setSuccess(123456);
+            }
+        });
+
+        // main 线程阻塞等待执行结果
+        try {
+            promise.sync();
+            // promise.await();
+        } catch (InterruptedException e) {
+        }
+    }
+}
